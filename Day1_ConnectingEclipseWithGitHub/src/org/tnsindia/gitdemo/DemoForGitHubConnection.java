addd36f0a05fbd71184cd85e59cbc47182e1b969
DELL <DELL@DESKTOP-OO854PV>
2022-11-16 19:42:05 +0530

    First commit

---
+++ Day1_ConnectingEclipseWithGitHub/src/org/tnsindia/gitdemo/DemoForGitHubConnection.java
@@ -0,0 +1,10 @@
+package org.tnsindia.gitdemo;
+
+public class DemoForGitHubConnection {
+
+	public static void main(String[] args) {
+		
+        System.out.println("welcome to tns india foundation");
+	}
+
+}
