d0ff295f20bf2097ba5e0c30741e5a322ecbb0ff
DELL <DELL@DESKTOP-OO854PV>
2022-11-29 19:10:59 +0530

    first commit

---
+++ day8_finalandsuperkeyword/src/org/tnsindia/finalkeyword/FinalChildClass.java
@@ -0,0 +1,14 @@
+package org.tnsindia.finalkeyword;
+//child class
+public class FinalChildClass extends FinalParentClass{
+	
+	//method
+	public void print()
+	{
+		System.out.println(name);
+	}
+	
+	
+	
+
+}
---
+++ day8_finalandsuperkeyword/src/org/tnsindia/finalkeyword/FinalClassExecutor.java
@@ -0,0 +1,10 @@
+package org.tnsindia.finalkeyword;
+
+public class FinalClassExecutor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
