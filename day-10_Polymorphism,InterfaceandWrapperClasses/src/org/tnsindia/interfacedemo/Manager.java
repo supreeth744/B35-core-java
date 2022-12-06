8ca0788c597fd240b068ef38b6c2978060d20e99
DELL <DELL@DESKTOP-OO854PV>
2022-12-07 01:38:53 +0530

    First commit

---
+++ day-10_Polymorphism,InterfaceandWrapperClasses/src/org/tnsindia/interfacedemo/Manager.java
@@ -0,0 +1,13 @@
+package org.tnsindia.interfacedemo;
+/*//INTERFACE 2
+//Any interface extends the multiple interfaces but not the classes
+public interface Manager extends HR{
+	//abstract method
+	void projectDiscussion();
+}*/
+//interface-2
+public interface Manager
+{
+	//abstract method
+	void projectDiscussion();
+}
